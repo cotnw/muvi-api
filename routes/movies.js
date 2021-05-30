9dac04b098245bc56c0706fd8d8d5ef19330409f
Anshul Saha <anshulsaha15@gmail.com>
2021-05-30 23:09:08 +0530

    404 error handling

--- routes/movies.js
+++ routes/movies.js
@@ -12,31 +12,35 @@ router.get('/', async(req, res) => {
         source: 'YTS Torrents'
     }
     let response = await axios.get(`https://yts.mx/api/v2/list_movies.json?query_term=${req.query.q}`)
-    console.log(response.data.movies)
-    response.data.data.movies.forEach((movie, index) => {
-        movie.torrents.forEach(torrent => {
-            movieObject.torrents.push({
-                url: torrent.url,
-                seeds: torrent.seeds,
-                peers: torrent.peers,
-                quality: `${torrent.quality} ${torrent.type}`,
-                size: torrent.size,
-                magnet: `magnet:?xt=urn:btih:${torrent.hash}&dn=${encodeURIComponent(movie.title)}`
+    console.log(response.data.data.movies)
+    if(response.data.data.movies == undefined) {
+        res.sendStatus(404)
+    } else {
+        response.data.data.movies.forEach((movie, index) => {
+            movie.torrents.forEach(torrent => {
+                movieObject.torrents.push({
+                    url: torrent.url,
+                    seeds: torrent.seeds,
+                    peers: torrent.peers,
+                    quality: `${torrent.quality} ${torrent.type}`,
+                    size: torrent.size,
+                    magnet: `magnet:?xt=urn:btih:${torrent.hash}&dn=${encodeURIComponent(movie.title)}`
+                })
             })
+            movieObject.title = movie.title
+            movieObject.link = movie.url
+            movieObject.poster_url = movie.medium_cover_image
+            moviesList.push(movieObject)
+            movieObject = {
+                title: '',
+                poster_url: '',
+                link: '',
+                torrents: [],
+                source: 'YTS Torrents'
+            }
         })
-        movieObject.title = movie.title
-        movieObject.link = movie.url
-        movieObject.poster_url = movie.medium_cover_image
-        moviesList.push(movieObject)
-        movieObject = {
-            title: '',
-            poster_url: '',
-            link: '',
-            torrents: [],
-            source: 'YTS Torrents'
-        }
-    })
-    res.json(moviesList)
+        res.json(moviesList)
+    }
 });
 
 module.exports = router
