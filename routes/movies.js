09eac1af73d1fd6e5d5b450a5150691bf1486d5a
Laxya Pahuja <laxya.pahuja8@gmail.com>
2021-05-30 21:01:32 +0530

    please

--- routes/movies.js
+++ routes/movies.js
@@ -4,8 +4,38 @@ const YTS = require('../sources/yts')
 
 router.get('/', async(req, res) => {
     moviesList = []
-    let yts = await YTS(req.query.q)
-    moviesList.push.apply(moviesList, yts)
+    movieObject = {
+        title: '',
+        poster_url: '',
+        link: '',
+        torrents: [],
+        source: 'YTS Torrents'
+    }
+    let response = await axios.get(`https://yts.mx/api/v2/list_movies.json?query_term=${query}`)
+    console.log(response.data.movies)
+    response.data.data.movies.forEach((movie, index) => {
+        movie.torrents.forEach(torrent => {
+            movieObject.torrents.push({
+                url: torrent.url,
+                seeds: torrent.seeds,
+                peers: torrent.peers,
+                quality: `${torrent.quality} ${torrent.type}`,
+                size: torrent.size,
+                magnet: `magnet:?xt=urn:btih:${torrent.hash}&dn=${encodeURIComponent(movie.title)}`
+            })
+        })
+        movieObject.title = movie.title
+        movieObject.link = movie.url
+        movieObject.poster_url = movie.medium_cover_image
+        movieHits.push(movie)
+        movieObject = {
+            title: '',
+            poster_url: '',
+            link: '',
+            torrents: [],
+            source: 'YTS Torrents'
+        }
+    })
     res.json(moviesList)
 });
 
