702b31043b2da06ba0e5efac6c248b00142c6bd1
Laxya Pahuja <laxya.pahuja8@gmail.com>
2021-05-30 20:40:37 +0530

    yts

--- routes/movies.js
+++ routes/movies.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const yts = require('../sources/yts')
+const YTS = require('../sources/yts')
 
 router.get('/', async(req, res) => {
     moviesList = []
-    let yts = await yts(req.query.q)
+    let yts = await YTS(req.query.q)
     moviesList.push.apply(moviesList, yts)
     res.json(moviesList)
 });
